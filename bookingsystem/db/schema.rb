5bc7cb13cbfe435e52e0da468d2efd3a8f302627
unknown <suwage@spartaglobal.com>
2018-07-31 10:31:19 +0100

    Admin panel working with user and admin access

--- bookingsystem/db/schema.rb
+++ bookingsystem/db/schema.rb
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 2018_07_30_101415) do
     t.string "encrypted_password", default: "", null: false
     t.string "name"
     t.string "lastname"
+    t.boolean "admin"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
