be488f0f33dbbb56a0c6d250cd71f242044cef43
unknown <suwage@spartaglobal.com>
2018-07-31 13:55:51 +0100

    Seed file has been updated with admin login

--- bookingsystem/db/seeds.rb
+++ bookingsystem/db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+user = User.new
+user.name = 'Admin'
+user.email = 'admin@admin.com'
+user.password = '123456'
+user.admin = true
+user.save!
