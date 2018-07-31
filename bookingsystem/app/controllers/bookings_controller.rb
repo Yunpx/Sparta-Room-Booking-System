7cda4246f8d2e8f71b410ffe2e9408a148a52245
unknown <Scampbell@spartaglobal.com>
2018-07-31 10:52:02 +0100

    CONTROLLER: Corrected the validation of each field in the f

--- bookingsystem/app/controllers/bookings_controller.rb
+++ bookingsystem/app/controllers/bookings_controller.rb
@@ -69,6 +69,6 @@ class BookingsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def booking_params
-      params.require(:booking).permit(:user_id, :room_id)
+      params.require(:booking).permit(:name, :email,:room_name, :participants, :date, :starttime, :duration, :importance, :note)
     end
 end
