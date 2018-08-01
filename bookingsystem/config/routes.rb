c81942638534cf82f940c7b0ed4434cd344fa480
unknown <Scampbell@spartaglobal.com>
2018-08-01 13:40:53 +0100

    SPRINT 1 DONEZO

--- bookingsystem/config/routes.rb
+++ bookingsystem/config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :rooms
 
 
-  root to: 'rooms#index'
+  root to: 'bookings#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
