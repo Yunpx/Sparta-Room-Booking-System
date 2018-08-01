361ff2eba3ffa293ba3dda9662e614ecb7aaf120
unknown <jskyers@spartaglobal.com>
2018-08-01 18:48:49 +0100

    seed file and migration done

--- bookingsystem/db/schema.rb
+++ bookingsystem/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_01_154502) do
+ActiveRecord::Schema.define(version: 2018_08_01_170254) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,10 +29,13 @@ ActiveRecord::Schema.define(version: 2018_08_01_154502) do
     t.integer "user_id"
     t.string "status"
     t.string "email"
-    
-    t.integer "day_id"
-
     t.string "category"
+    t.integer "day_id"
+  end
+
+  create_table "days", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "rooms", force: :cascade do |t|
